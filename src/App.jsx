6660f23d5a5e24df58a80c5b6a82dd9c5042f1e8
akhmedovkb <akhmedovkb@gmail.com>
2025-07-10 20:08:27 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RegistrationForm from './components/RegistrationForm';
-// import Login from './pages/Login';
+// import PrivateRoute from './pages/PrivateRoute';
 // import Dashboard from './pages/Dashboard';
-import PrivateRoute from './pages/PrivateRoute';
+// import Login from './pages/Login';
 
 function App() {
   return (
@@ -11,14 +10,7 @@ function App() {
       <Routes>
         <Route path="/register" element={<RegistrationForm />} />
         {/* <Route path="/login" element={<Login />} /> */}
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
+        {/* <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} /> */}
         <Route path="*" element={<div>404 — Страница не найдена</div>} />
       </Routes>
     </Router>
