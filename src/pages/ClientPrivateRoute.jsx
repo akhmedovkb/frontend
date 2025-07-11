0dc8b0b64241564d50c4e061215fdcf914aa352c
akhmedovkb <akhmedovkb@gmail.com>
2025-07-11 16:54:40 +0500

    Create ClientPrivateRoute.jsx

---
+++ src/pages/ClientPrivateRoute.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+function ClientPrivateRoute({ children }) {
+  const token = localStorage.getItem('clientToken');
+
+  return token ? children : <Navigate to="/client-login" />;
+}
+
+export default ClientPrivateRoute;
