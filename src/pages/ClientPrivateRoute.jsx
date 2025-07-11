536bb793c20dfbd04545ca451f569ac33e5d6746
akhmedovkb <akhmedovkb@gmail.com>
2025-07-11 22:21:23 +0500

    Update ClientPrivateRoute.jsx

--- src/pages/ClientPrivateRoute.jsx
+++ src/pages/ClientPrivateRoute.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-const ClientPrivateRoute = ({ children }) => {
+function ClientPrivateRoute({ children }) {
   const token = localStorage.getItem('clientToken');
-  return token ? children : <Navigate to="/client/login" />;
-};
+
+  if (!token) {
+    return <Navigate to="/client/login" />;
+  }
+
+  return children;
+}
 
 export default ClientPrivateRoute;
