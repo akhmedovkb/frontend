98ffd298d5a4266f9d4fbdff581dded1685a3ed7
akhmedovkb <akhmedovkb@gmail.com>
2025-07-11 08:09:59 +0500

    Update PrivateRoute.jsx

--- src/pages/PrivateRoute.jsx
+++ src/pages/PrivateRoute.jsx
@@ -1,9 +1,11 @@
 // src/pages/PrivateRoute.jsx
+import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-function PrivateRoute({ children }) {
+const PrivateRoute = ({ children }) => {
   const token = localStorage.getItem('token');
-  return token ? children : <Navigate to="/login" />;
-}
+  return token ? children : <Navigate to="/login" replace />;
+};
 
 export default PrivateRoute;
+
