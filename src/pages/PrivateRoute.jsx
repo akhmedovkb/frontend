e86d8600749883df76f2eb944de739f7d3f6a0bb
akhmedovkb <akhmedovkb@gmail.com>
2025-07-12 13:06:14 +0500

    Update PrivateRoute.jsx

--- src/pages/PrivateRoute.jsx
+++ src/pages/PrivateRoute.jsx
@@ -1,24 +1,10 @@
-// frontend/src/pages/PrivateRoute.jsx
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 function PrivateRoute({ children }) {
-  const [loading, setLoading] = useState(true);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const token = localStorage.getItem('providerToken');
 
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      setIsAuthenticated(true);
-    }
-    setLoading(false);
-  }, []);
-
-  if (loading) {
-    return <div className="text-center mt-10">Загрузка...</div>;
-  }
-
-  if (!isAuthenticated) {
+  if (!token) {
     return <Navigate to="/login" replace />;
   }
 
