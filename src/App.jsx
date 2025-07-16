0a587c1bb49cfa0c68b574da2252a5c10a1f4497
akhmedovkb <akhmedovkb@gmail.com>
2025-07-16 17:54:39 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import RegistrationForm from './components/RegistrationForm';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import PrivateRoute from './pages/PrivateRoute';
-import ClientRegister from './pages/ClientRegister';
-import ClientLogin from './pages/ClientLogin';
-import ClientDashboard from './pages/ClientDashboard';
-import ClientPrivateRoute from './pages/ClientPrivateRoute';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import PrivateRoute from "./pages/PrivateRoute";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/register" element={<RegistrationForm />} />
         <Route path="/login" element={<Login />} />
-
         <Route
           path="/dashboard"
           element={
@@ -24,27 +16,7 @@ function App() {
             </PrivateRoute>
           }
         />
-
-        <Route path="/client/register" element={<ClientRegister />} />
-        <Route path="/client/login" element={<ClientLogin />} />
-        <Route
-          path="/client/dashboard"
-          element={
-            <ClientPrivateRoute>
-              <ClientDashboard />
-            </ClientPrivateRoute>
-          }
-        />
-
-        {/* Страница 404 */}
-        <Route
-          path="*"
-          element={
-            <div className="text-center mt-10 text-xl">
-              404 — Страница не найдена
-            </div>
-          }
-        />
+        <Route path="*" element={<div>404 — Страница не найдена</div>} />
       </Routes>
     </Router>
   );
